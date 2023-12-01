1de3e2f0862d62808b40781a563275df18329e86
Lars Knoke <l.knoke@colorplus.de>
2023-12-01 15:41:42 +0100

    Texte "Ende"

--- src/components/close-text.js
+++ src/components/close-text.js
@@ -19,33 +19,23 @@ function CloseText() {
             size={"md"}
             textAlign={"center"}
             mb={"6"}
+            lineHeight={8}
           >
-            Die Bewerbungsphase ist beendet
+            Die Bewerbungsphase ist beendet - <br />
+            eine Bewerbung für den 11. Town & Country Stiftungspreis nicht mehr
+            möglich.
           </Heading>
-          <Text color={"gray.500"} mt={"4"}>
-            Vielen Dank für das große Interesse am 11. Town & Country
-            Stiftungspreis und die zahlreichen Bewerbungen, die uns erreicht
-            haben! Die Resonanz auf unsere Ausschreibung war beeindruckend.
-            Gleichzeitig gingen weit mehr Bewerbungen ein, als gefördert werden
-            können. Mit dem 11. Stiftungspreis fördern wir bundesweit 300
-            Projekte. Daher haben wir uns dazu entschieden, das
-            Bewerbungsverfahren vorzeitig zu schließen.
+          <Text color={"gray.500"} mt={"6"} as={"b"} textAlign={"center"}>
+            Vielen Dank für die zahlreichen Bewerbungen, die uns erreicht haben!
           </Text>
-          <Text color={"gray.500"} mt={"6"} as={"b"}>
-            Aufgrund der hohen Nachfrage ist eine Bewerbung für den 11. Town &
-            Country Stiftungspreis nicht mehr möglich.
-          </Text>
-          <Text color={"gray.500"} mt={"6"}>
+          <Text color={"gray.500"} mt={"6"} textAlign={"center"}>
             Die Preisträger:innen werden Anfang Februar 2024 auf der Webseite
-            der Stiftung öffentlich bekannt gegeben und von ihren
-            Botschafter:innen der Stiftung informiert.
+            der Stiftung{" "}
+            <a href="https://www.tc-stiftung.de">www.tc-stiftung.de</a>{" "}
+            öffentlich bekannt gegeben und von ihren Botschafter:innen der
+            Stiftung informiert.
           </Text>
-          <Text color={"gray.500"} mt={"4"}>
-            Wir bitten alle, die in diesem Jahr nicht berücksichtigt werden
-            können, um Verständnis und freuen uns auf eine Teilnahme beim 12.
-            Stiftungspreis.
-          </Text>
-          <Text color={"gray.500"} mt={"4"}>
+          <Text color={"gray.500"} mt={"4"} textAlign={"center"}>
             Gern steht Ihnen das Team der Town & Country Stiftung für weitere
             Fragen unter der Rufnummer 0361 / 644 789 14 oder per E-Mail:{" "}
             <a href="mailto:stiftungspreis@tc-stiftung.de">
