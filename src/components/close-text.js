b70c31236502864fafc6eda234a68b488c271401
Lars Knoke <larz.knoke@gmail.com>
2025-10-31 22:31:13 +0100

    closer

--- src/components/close-text.js
+++ src/components/close-text.js
@@ -22,26 +22,34 @@ function CloseText() {
             lineHeight={8}
             px={{ base: 2, md: 4 }}
           >
-            Ab dem 01. September könnt Ihr Euch mit Eurem Projekt auf den 12.
-            Town &amp; Country Stiftungspreis oder den dazugehörigen Sonderpreis
-            zum Motto „Jung trifft Alt - Begegnung, die verbindet“ hier
-            bewerben.
+            Die Bewerbungsphase für den 12. Town & Country Stiftungspreis ist
+            beendet.
           </Heading>
-          <Text as={"b"} color={"gray.500"} mt={"6"} textAlign={"center"}>
-            Weitere Informationen findet Ihr auf unserer Website{" "}
-            <a href="https://www.tc-stiftung.de">www.tc-stiftung.de</a>{" "}
+          <Text color={"gray.500"} mt={"4"} textAlign={"center"}>
+            Vielen Dank für die zahlreichen Bewerbungen und Euer Interesse am
+            12. Town & Country Stiftungspreis! <br /> Wir freuen uns über die
+            bundesweit agierenden sowie wirkungsvollen Projekte für Kinder- und
+            Jugendhilfe.
           </Text>
           <Text color={"gray.500"} mt={"4"} textAlign={"center"}>
-            Gerne steht Euch das Team der Town &amp; Country Stiftung für
-            weitere Fragen <br /> unter der Rufnummer 0361 / 644 789 14 oder per
-            Mail{" "}
+            Die Preisträger:innen werden Ende Januar 2026 auf der Webseite der
+            Stiftung bekannt gegeben.
+          </Text>
+          <Text color={"gray.500"} mt={"4"} textAlign={"center"}>
+            Alle diejenigen, die in diesem Jahr nicht berücksichtigt werden
+            konnten, bitten wir um Verständnis. <br />
+            Es gibt unzählige wertvolle Projekte und wir müssen eine Auswahl
+            treffen!
+          </Text>
+          <Text color={"gray.500"} mt={"4"} textAlign={"center"}>
+            Wir, das Team der Town & Country Stiftung, beantworten gern Eure
+            Fragen: <br /> Telefon 0361/ 644 789 14 oder per E-Mail:{" "}
             <a href="mailto:stiftungspreis@tc-stiftung.de">
               stiftungspreis@tc-stiftung.de
             </a>{" "}
-            zur Verfügung.
           </Text>
           <Text as={"b"} color={"gray.500"} mt={"8"} textAlign={"center"}>
-            Wir freuen uns auf Eure Bewerbung! <br />
+            Eure Town & Country Stiftung
           </Text>
         </VStack>
       </CardBody>{" "}
