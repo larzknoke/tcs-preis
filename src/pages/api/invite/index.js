17de10ccee6097db31f7c13da59d8193b5ba6a94
Lars Knoke <l.knoke@colorplus.de>
2026-02-05 11:33:10 +0100

    kampagne für anmeldung

--- src/pages/api/invite/index.js
+++ src/pages/api/invite/index.js
@@ -16,7 +16,11 @@ export default async function handle(req, res) {
       const data = req.body;
       console.log("data: ", data);
 
-      const kampagne = await prisma.kampagne.findFirst();
+      const kampagne = await prisma.kampagne.findFirst({
+        where: {
+          abgeschlossen: false,
+        },
+      });
       data.kampagneId = kampagne ? kampagne.id : null;
 
       const result = await prisma.invite.create({ data: data });
